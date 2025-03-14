c35d99d6f57423b8d5a07693890354b61cc1311d
Ngo Ky Anh <kyanh2501@gmail.com>
2025-03-14 15:02:25 +0700

    [UPDATE] Remove unused components

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from './components/ui/provider'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
-  </StrictMode>,
-)
+    </ChakraProvider>
+  </StrictMode>
+);
