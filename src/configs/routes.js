15864a766c45aee11a12d98e713d39026f268acd
Ngo Ky Anh <kyanh2501@gmail.com>
2025-03-24 16:58:52 +0700

    Revert "Souf"

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -7,10 +7,7 @@ import HomePage from '@/pages/HomePage';
 import LoginPage from '@/pages/LoginPage';
 import MembershipPage from '@/pages/MembershipPage';
 import GrowthChartPage from '@/pages/GrowthChartPage';
-import DoctorRequestPage from '@/pages/DoctorRequestPage';
-import ChildDetail from '@/components/Child/ChildDetail';
-import ConsultationPage from '@/pages/ConsultationPage';
-import ConsultationChatPage from '@/pages/ConsultationChatPage';
+
 const routes = [
   {
     layout: MainLayout,
@@ -22,20 +19,6 @@ const routes = [
         title: 'Home',
       },
       {
-        path: '/doctor/requests',
-        component: DoctorRequestPage,
-        title: 'Doctor Requests',
-        shouldLogin: true,
-        role: 2
-      },
-      // {
-      //   path: '/doctor/consultations',
-      //   component: DoctorRequestPage, // Thay bằng ConsultationPage khi có
-      //   title: 'Consultations',
-      //   shouldLogin: true,
-      //   role: 2
-      // },
-      {
         path: '/plans',
         component: MembershipPage,
         title: 'Membership',
@@ -45,40 +28,17 @@ const routes = [
         component: ChildPage,
         title: 'Child Management',
         shouldLogin: true,
-        role: 1
-      },
-      {
-        path: '/children/:childId',
-        component: ChildDetail,
-        title: 'Child Detail',
-        shouldLogin: true,
-        role: 1
       },
       {
         path: '/growth-charts',
         component: GrowthChartPage,
         title: 'Growth Charts',
-        shouldLogin: true,
-        role: 1
       },
       {
         path: '/growth-charts/:childId',
         component: GrowthChartPage,
         title: 'Growth Charts',
-        shouldLogin: true,
-        role: 1
       },
-      {
-        path: '/consultations',
-        component: ConsultationPage,
-        title: 'Consultations',
-      },
-      {
-        path: '/consultation-chat/:consultationId',
-        component: ConsultationChatPage, 
-        title: 'Consultation Chat Detail',
-      },
-      
     ],
   },
   {
